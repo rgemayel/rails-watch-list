bd5b9e36ca8f4b28e2baf575fed11ad79c921217
Romance Gemayel <25gemayelromance@gmail.com>
2023-11-23 17:55:48 +0200

    Adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/lists", to: "lists#index"
+  get "/lists/new", to: "lists#new"
+  get "/lists/:id", to: "lists#show", as: "list"
 end
